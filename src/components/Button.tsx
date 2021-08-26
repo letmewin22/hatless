04d0966b834971808b9a8013cbe3ecca11d2c329
letmewin22 <lkosteckiy5@gmail.com>
2021-08-26 14:03:21 +0300

    navbar completed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,5 +8,10 @@ interface IProps {
 export const Button: React.FC<IProps> = ({ text, color }) => {
   const colorClass = color ? `btn--${color}` : 'btn--primary'
   const classes = ['btn', colorClass]
-  return <button className={classes.join(' ')}>{text}</button>
+  return (
+    <button className={classes.join(' ')}>
+      <span>{text}</span>
+      <img src="/rocket.png" alt="button icon" className="btn__icon" />
+    </button>
+  )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,13 +35,46 @@ export const Navbar: React.FC = () => {
     },
   ]
 
+  const navbarSocial = [
+    {
+      link: 'https://facebook.com',
+      label: 'facebook',
+      icon: '/social/fb.svg',
+    },
+    {
+      link: 'https://lenkedin.com',
+      label: 'lenkedin',
+      icon: '/social/linkedin.svg',
+    },
+    {
+      link: 'https://twitter.com',
+      label: 'twitter',
+      icon: '/social/twitter.svg',
+    },
+    {
+      link: 'https://instagram.com',
+      label: 'instagram',
+      icon: '/social/insta.svg',
+    },
+  ]
+
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const burgerHandler = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <div className="navbar">
       <div className="navbar__container">
         <Link to="/" className="navbar__logo">
           <img src="/logo.svg" alt="logo" className="navbar__logo-img" />
         </Link>
-        <div className="navbar__right">
+        <div
+          className={
+            isOpen ? 'navbar__right navbar__right--opened' : 'navbar__right'
+          }
+        >
           <nav className="navbar__nav navbar-nav">
             <ul className="navbar-nav__items">
               {navbarItems.map(item => (
@@ -65,7 +98,34 @@ export const Navbar: React.FC = () => {
             </ul>
           </nav>
           <Button text="Kickstart jouw project!" />
+          <ul className="navbar__social navbar-social">
+            {navbarSocial.map(el => (
+              <li key={el.link} className="navbar-social__item">
+                <a
+                  href={el.link}
+                  aria-label={el.label}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={el.icon} alt={el.label} />
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
+        <button
+          onClick={burgerHandler}
+          aria-label="Menu button"
+          className={
+            isOpen
+              ? 'navbar__burger burger burger--opened'
+              : 'navbar__burger burger'
+          }
+        >
+          <span className="burger__line"></span>
+          <span className="burger__line"></span>
+          <span className="burger__line"></span>
+        </button>
       </div>
     </div>
   )
